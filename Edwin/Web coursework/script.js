9fbf457e543a0ae4551121290032a4b82f7817ea
ctelo <125046918+ctelo@users.noreply.github.com>
2024-03-12 19:10:35 +0000

    Add files via upload

---
+++ Edwin/Web coursework/script.js
@@ -0,0 +1,25 @@
+const body = document.querySelector("body"),
+      sidebar = body.querySelector(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box");
+
+      toggle.addEventListener("click", () =>{
+        sidebar.classList.toggle("close");
+      });
+
+      searchBtn.addEventListener("click", () =>{
+        sidebar.classList.remove("close");
+      });
+
+      function clearCheckboxes(){
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+        checkboxes.forEach(function(checkbox){
+            checkbox.checked = false;
+        });
+      }
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var clearButton = document.querySelector('.clear');
+        clearButton.addEventListener('click', clearCheckboxes);
+      });
